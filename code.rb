346b19e447d03b98920bab83bb40e2ccf4a1be89
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 07:49:25 -0500

    Initial commit

---
+++ code.rb
@@ -0,0 +1,4 @@
+b="to "
+a= "the "
+c= "Today "
+puts  c + "we "+ "travelled " + b + a + " planet " + "Mars " + "and "+ "danced " + "on " + a +  "dust " + "while " + "listening " + b + " " + "classic " + "music."
